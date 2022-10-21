f71f6f8dd2f922286bb0a859dbf58deeb2005f15
IqraMahmood <87108359+IqraMahmood@users.noreply.github.com>
2022-10-21 12:28:13 -0700

    Add basic python programs

---
+++ 04_comments.py
@@ -0,0 +1,7 @@
+print("How are you") # Pree these to comment out (ctrl + /)
+# print("Hello world")
+# print("We are learning python with ammar")
+# print('Test for single quotes')
+# print("Test for doulble quote")
+# print("Whats up")
+
